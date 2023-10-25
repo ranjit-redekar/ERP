15a563813627ef275e3ebfdd09c5a3f1ffac461e
ranjit-redekar <ranjitredekar8@gmail.com>
2023-10-25 20:47:59 +0530

    Added dashboaed

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,18 @@
-import { ProCard, StatisticCard } from '@ant-design/pro-components';
+import { StatisticCard } from '@ant-design/pro-components';
 import RcResizeObserver from 'rc-resize-observer';
 import { useState } from 'react';
+import { PurschaseIcon, RupeeIcon, SaleIcon } from '../../common/Icons';
+import {
+  theme,
+} from "antd";
+import './Dashboard.scss';
 
 const { Statistic } = StatisticCard;
 
 export default () => {
   const [responsive, setResponsive] = useState(false);
-
+  const { token } = theme.useToken();
+console.log("AAAAAA", token)
   return (
     <RcResizeObserver
       key="resize-observer"
@@ -14,128 +20,46 @@ export default () => {
         setResponsive(offset.width < 596);
       }}
     >
-      <ProCard split={responsive ? 'horizontal' : 'vertical'}>
-        {/* <StatisticCard
-          colSpan={responsive ? 24 : 6}
-          title="Annual Performance Target"
-          statistic={{
-            value: 82.6,
-            suffix: 'Billion',
-            description: <Statistic title="Day-to-Day" value="6.47%" trend="up" />,
-          }}
-          chart={
-            <img
-              src="https://gw.alipayobjects.com/zos/alicdn/PmKfn4qvD/mubiaowancheng-lan.svg"
-              alt="Progress Bar"
-              width="100%"
-            />
-          }
-          footer={
-            <>
-              <Statistic
-                value="70.98%"
-                title="Annual Performance Completion Rate"
-                layout="horizontal"
-              />
-              <Statistic
-                value="86.98%"
-                title="Previous Year's Performance Completion Rate"
-                layout="horizontal"
-              />
-              <Statistic
-                value="88.98%"
-                title="Performance Completion Rate Two Years Ago"
-                layout="horizontal"
-              />
-            </>
-          }
-        /> */}
+      {/* <ProCard split={responsive ? 'horizontal' : 'vertical'}> */}
         <StatisticCard.Group
           colSpan={responsive ? 24 : 18}
           direction={responsive ? 'column' : undefined}
+          className='erp-StatisticCard-group'
         >
           <StatisticCard
+            className='erp-dashboard-card'
+            style={{ boxShadow: token.boxShadow }}
             statistic={{
-              title: 'Total Annual Revenue',
-              value: 601987768,
-              description: (
-                <Statistic title="Day-to-Day" value="6.15%" trend="up" />
-              ),
+              title: 'Sale',
+              value: 1000,
+              description: <Statistic title="Percentage" value="61.5%" />,
+              icon: <SaleIcon fill="blue" />,
+              // tip:"tip"
+              valueStyle:{color: 'red' }
             }}
-            chart={
-              <img
-                src="https://gw.alipayobjects.com/zos/alicdn/zevpN7Nv_/xiaozhexiantu.svg"
-                alt="Line Chart"
-                width="100%"
-              />
-            }
-          >
-            <Statistic
-              title="Market Total Revenue"
-              value={1982312}
-              layout="vertical"
-              description={
-                <Statistic title="Day-to-Day" value="6.15%" trend="down" />
-              }
-            />
-          </StatisticCard>
+          />
           <StatisticCard
+            className='erp-dashboard-card'
+            style={{ boxShadow: token.boxShadow }}
             statistic={{
-              title: 'Todays Ranking',
-              value: 6,
-              description: (
-                <Statistic title="Day-to-Day" value="3.85%" trend="down" />
-              ),
+              title: 'Purchase',
+              value: 10000,
+              description: <Statistic title="Percentage" value="61.5%" />,
+              icon:<PurschaseIcon fill="red" /> 
             }}
-            chart={
-              <img
-                src="https://gw.alipayobjects.com/zos/alicdn/zevpN7Nv_/xiaozhexiantu.svg"
-                alt="Line Chart"
-                width="100%"
-              />
-            }
-          >
-            <Statistic
-              title="Last 7 Days Revenue"
-              value={17458}
-              layout="vertical"
-              description={
-                <Statistic title="Day-to-Day" value="6.47%" trend="up" />
-              }
-            />
-          </StatisticCard>
+          />
           <StatisticCard
-          statistic={{
-            title: 'Paid Traffic',
-            value: 3701928,
-            description: <Statistic title="Percentage" value="61.5%" />,
-          }}
-          chart={
-            <img
-              src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
-              alt="Percentage"
-              width="100%"
-            />
-          }
-          chartPlacement="left"
-        />
-        <StatisticCard
-          statistic={{
-            title: 'Free Traffic',
-            value: 1806062,
-            description: <Statistic title="Percentage" value="38.5%" />,
-          }}
-          chart={
-            <img
-              src="https://gw.alipayobjects.com/zos/alicdn/6YR18tCxJ/huanlv.svg"
-              alt="Percentage"
-              width="100%"
-            />
-          }
-          chartPlacement="left"
-        />
+            className='erp-dashboard-card'
+            style={{ boxShadow: token.boxShadow }}
+            statistic={{
+              title: 'Profit',
+              value: 10000,
+              description: <Statistic title="Percentage" value="38.5%" />,
+              icon: <RupeeIcon fill="green" />
+            }}
+          />
         </StatisticCard.Group>
-      </ProCard>
+      {/* </ProCard> */}
     </RcResizeObserver>
   );
 };
